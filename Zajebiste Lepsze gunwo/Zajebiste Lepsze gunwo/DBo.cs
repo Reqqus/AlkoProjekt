63c3b734ab8d8f2a4667dbac52014b597cbc2a3e
reqqus <t_losek@poczta.wwsi.edu.pl>
2020-08-05 23:59:00 +0200

    Apdejcik

---
+++ Zajebiste Lepsze gunwo/Zajebiste Lepsze gunwo/DBo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zajebiste_Lepsze_gunwo
+{
+    public class DBo
+    {
+
+
+        public static void Connect()
+        {
+            string ConString = @"Data Source=sqltester2018.wwsi.edu.pl;Initial Catalog=TestLosek;Persist Security Info=True;User ID=tlosek;Password=Ulaniec!@#";
+            SqlConnection con;
+
+            con = new SqlConnection(ConString);
+
+            con.Open();
+
+            Console.WriteLine("Jest połączone kurdebele");
+
+            con.Close();
+            
+
+        }
+
+
+
+
+    }
+}
---
+++ Zajebiste Lepsze gunwo/Zajebiste Lepsze gunwo/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Zajebiste_Lepsze_gunwo
+{
+    class Program : DBo
+    {
+        static void Main(string[] args)
+        {
+            //Connect();
+
+             OdczytywanieDanych();
+
+            //dodawanie();
+            
+            
+        }
+
+
+        public static void Connect()
+        {
+            string ConString = @"Data Source=sqltester2018.wwsi.edu.pl;Initial Catalog=TestLosek;Persist Security Info=True;User ID=tlosek;Password=Ulaniec!@#";
+            SqlConnection con;
+
+            con = new SqlConnection(ConString);
+
+            con.Open();
+
+            Console.WriteLine("Jest połączone kurdebele");
+
+            con.Close();
+
+
+        }
+
+
+        public static void OdczytywanieDanych ()
+        {
+            string ConString = @"Data Source=sqltester2018.wwsi.edu.pl;Initial Catalog=TestLosek;Persist Security Info=True;User ID=tlosek;Password=Ulaniec!@#";
+
+            SqlConnection con;
+
+            con = new SqlConnection(ConString);
+
+            con.Open();
+
+            SqlCommand cmd;
+            SqlDataReader reader;
+
+            string sql, output = "";
+
+            sql = "select * from Imiona";
+
+            cmd = new SqlCommand(sql, con);
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+
+            {
+                output = output + reader.GetValue(0) + " - "   + reader.GetValue(1) + "\n";
+                
+
+            }
+            Console.WriteLine(output);
+            Console.ReadLine();
+            reader.Close();
+            cmd.Dispose();
+            con.Close();
+
+
+        }
+
+        public static void dodawanie()
+        {
+            string conString = @"Data Source=sqltester2018.wwsi.edu.pl;Initial Catalog=TestLosek;Persist Security Info=True;User ID=tlosek;Password=Ulaniec!@#";
+            SqlConnection con;
+            con = new SqlConnection(conString);
+            con.Open();
+            SqlCommand cmd;
+            SqlDataAdapter adapter;
+            string sql = "insert into imiona values('3','2')";
+            cmd = new SqlCommand(sql, con);
+            adapter = new SqlDataAdapter();
+            adapter.InsertCommand = cmd;
+            adapter.InsertCommand.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
+        }
+}
